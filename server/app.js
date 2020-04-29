2b1c17b70efe8ff11461c0fa3a5acbbcca0e2913
Zoilo Granda <zgranda@gmail.com>
2020-04-29 08:53:25 -0400

    User Model

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const PORT  = 5000
 const mongoose = require('mongoose');
 const {MONGO_URI} = require('./keys');
 
+require('./models/user')
+
 mongoose.connect(MONGO_URI, {
  useNewUrlParser:true,
  useUnifiedTopology:true
