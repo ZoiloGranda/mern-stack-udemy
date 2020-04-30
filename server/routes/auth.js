13fd9d380572375c7a89b769d2d4206af6f9b956
Zoilo Granda <zgranda@gmail.com>
2020-04-30 11:21:42 -0400

    Signin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,4 +32,27 @@ router.post('/signup', (req, res) => {
  })
 })
 
+router.post('/signin', (req, res)=>{
+ const {email, password} = req.body
+ if (!email || !password) {
+  return res.status(422).json({error:"Please provide email and password"})
+ }
+ User.findOne({email:email})
+ .then(savedUser=>{
+  if (!savedUser) {
+   return res.status(422).json({error:"Invalid email or password"})
+  }
+  bcrypt.compare(password,savedUser.password)
+  .then(doMatch=>{
+   if (doMatch) {
+    res.json({message:"successfully signed in"})
+   }else {
+    return res.status(422).json({error:"Invalid email or password"})
+   }
+  }).catch(err=>{
+   console.log(err);
+  })
+ })
+})
+
 module.exports = router
