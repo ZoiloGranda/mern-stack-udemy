cb688618a5f1b2874f8a437f686b9fe51c35d7cc
Zoilo Granda <zgranda@gmail.com>
2020-05-05 17:23:37 -0400

    Initial react commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+function App() {
+ return (<BrowserRouter>
+  <NavBar/>
+  <Route path="/"></Route>
+ </BrowserRouter>);
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavBar = () => {
+ return (<nav>
+  <div className="nav-wrapper white" style={{color:"black"}}>
+   <a href="#" className="brand-logo left">Logo</a>
+   <ul id="nav-mobile" className="right">
+    <li>
+     <a href="sass.html">Login</a>
+    </li>
+    <li>
+     <a href="badges.html">Signup</a>
+    </li>
+    <li>
+     <a href="collapsible.html">Profile</a>
+    </li>
+   </ul>
+  </div>
+ </nav>)
+}
+
+export default NavBar
