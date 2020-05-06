78980819f8dadce070de8ffb3aa91a7e810b4b05
Zoilo Granda <zgranda@gmail.com>
2020-05-06 09:32:10 -0400

    Signin and Signup pages

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-const Signup = () => {
+const Signin = () => {
  return (<div className="mycard">
   <div className="card auth-card input-field">
    <h2>Instaclone</h2>
-   <input type="text" placeholder="Name"/>
    <input type="text" placeholder="Email"/>
    <input type="password" placeholder="Password"/>
-   <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Signup
+   <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Login
    </button>
    <h5>
-    <Link to="/signin">Already have an account?</Link>
+    <Link to="/signup">Don't have an account?</Link>
    </h5>
   </div>
  </div>)
 }
 
-export default Signup
+export default Signin
