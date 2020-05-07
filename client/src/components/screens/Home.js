10cb00f8f30fa937523887dde73307378e2002b2
Zoilo Granda <zgranda@gmail.com>
2020-05-07 13:12:39 -0400

    Home Page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Home =()=>{
  return(
-  <h1>Home</h1>
+  <div className="home">
+   <div className="card home-card">
+    <h5>Zoilo</h5>
+    <div className="card-image">
+     <img src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="wallpaper"/>
+    </div>
+    <div className="card-content">
+     <i className="material-icons" style={{color:'red'}}>favorite</i>
+     <h6>Title</h6>
+     <p>Amazing Post</p>
+     <input type="text" placeholder="Add a comment"/>
+    </div>
+   </div>
+   <div className="card home-card">
+    <h5>Zoilo</h5>
+    <div className="card-image">
+     <img src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="wallpaper"/>
+    </div>
+    <div className="card-content">
+     <i className="material-icons" style={{color:'red'}}>favorite</i>
+     <h6>Title</h6>
+     <p>Amazing Post</p>
+     <input type="text" placeholder="Add a comment"/>
+    </div>
+   </div>
+   <div className="card home-card">
+    <h5>Zoilo</h5>
+    <div className="card-image">
+     <img src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="wallpaper"/>
+    </div>
+    <div className="card-content">
+     <i className="material-icons" style={{color:'red'}}>favorite</i>
+     <h6>Title</h6>
+     <p>Amazing Post</p>
+     <input type="text" placeholder="Add a comment"/>
+    </div>
+   </div>
+  </div>
  )
 }
 
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -30,7 +30,6 @@ const Profile =()=>{
     <img className="item" alt="pic here" src="https://images.unsplash.com/photo-1519176407-e10ad52ab0a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
     <img className="item" alt="pic here" src="https://images.unsplash.com/photo-1519176407-e10ad52ab0a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
     <img className="item" alt="pic here" src="https://images.unsplash.com/photo-1519176407-e10ad52ab0a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-    
    </div>
   </div>
  )
