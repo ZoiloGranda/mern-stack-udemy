5c9c40abd129524e5b999822db303d5127b146ba
Zoilo Granda <zgranda@gmail.com>
2020-05-08 17:18:12 -0400

    User Signup

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import M from 'materialize-css';
 
 const Signup = () => {
+ const history = useHistory();
+ const [name, setName] = useState('');
+ const [password, setPassword] = useState('');
+ const [email, setEmail] = useState('');
+ const PostData = () => {
+  if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+   M.toast({html: 'Invalid Email', classes: 'red darken-3'})
+   return
+  }
+  fetch('/signup', {
+   method: 'post',
+   headers: {
+    'Content-Type': 'application/json'
+   },
+   body: JSON.stringify({name, email, password})
+  }).then(res => res.json()).then(data => {
+   if (data.error) {
+    M.toast({html: data.error, classes: 'red darken-3'})
+   } else {
+    M.toast({html: data.message, classes: 'green darken-1'})
+    history.push('/signin')
+   }
+  })
+ }
+
  return (<div className="mycard">
   <div className="card auth-card input-field">
    <h2>Instaclone</h2>
-   <input type="text" placeholder="Name"/>
-   <input type="text" placeholder="Email"/>
-   <input type="password" placeholder="Password"/>
-   <button className="btn waves-effect waves-light  blue darken-1">Signup
+   <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+   <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+   <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+   <button className="btn waves-effect waves-light blue darken-1" onClick={() => PostData()}>Signup
    </button>
    <h5>
     <Link to="/signin">Already have an account?</Link>
