49b080dbe288dbfba87b526666a8b603b7965ecd
Zoilo Granda <zgranda@gmail.com>
2020-05-10 10:34:43 -0400

    Changing navigation links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
 
 const NavBar = () => {
- return (<nav>
-  <div className="nav-wrapper white" style={{color:"black"}}>
-   <Link to="/" className="brand-logo left">Instaclone</Link>
-   <ul id="nav-mobile" className="right">
-    <li>
-     <Link to="/signin">Signin</Link>
-    </li>
-    <li>
-     <Link to="/signup">Signup</Link>
-    </li>
-    <li>
+ const {state, dispatch} = useContext(UserContext)
+ const renderList = () => {
+  if (state) {
+   return [
+    <li key="profile">
      <Link to="/profile">Profile</Link>
-    </li>
-    <li>
+    </li>,
+    <li key="create">
      <Link to="/create">Create Post</Link>
     </li>
+   ]
+  } else {
+   return [
+    <li key="signin">
+     <Link to="/signin">Signin</Link>
+    </li>,
+    <li key="signup">
+     <Link to="/signup">Signup</Link>
+    </li>
+   ]
+  }
+ }
+ return (<nav>
+  <div className="nav-wrapper white" style={{
+    color: "black"
+   }}>
+   <Link to={state?"/":"signin"} className="brand-logo left">Instaclone</Link>
+   <ul id="nav-mobile" className="right">
+    {renderList()}
    </ul>
   </div>
  </nav>)
