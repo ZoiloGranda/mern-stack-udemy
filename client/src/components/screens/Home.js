3793ae833b852dc5441b4bdd5dc66e871ae8913a
Zoilo Granda <zgranda@gmail.com>
2020-05-12 13:25:33 -0400

    Comment frontend

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -13,53 +13,68 @@ const Home = () => {
    setData(result.posts)
   })
  }, [])
- 
- const likePost = (id)=>{
-  fetch('/like',{
-   method:'put',
-   headers:{
+
+ const likePost = (id) => {
+  fetch('/like', {
+   method: 'put',
+   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer '+localStorage.getItem('jwt')
+    Authorization: 'Bearer ' + localStorage.getItem('jwt')
    },
-   body:JSON.stringify({
-    postId:id
-   })
-  }).then(res=>res.json())
-  .then(result=>{
-   const newData = data.map(item=>{
-    if (item._id===result._id){
+   body: JSON.stringify({postId: id})
+  }).then(res => res.json()).then(result => {
+   const newData = data.map(item => {
+    if (item._id === result._id) {
      return result
     } else {
      return item
     }
    })
    setData(newData)
-  }).catch(err=>{
+  }).catch(err => {
    console.log(err);
   })
  }
- 
- const unlikePost = (id)=>{
-  fetch('/unlike',{
-   method:'put',
-   headers:{
+
+ const unlikePost = (id) => {
+  fetch('/unlike', {
+   method: 'put',
+   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer '+localStorage.getItem('jwt')
+    Authorization: 'Bearer ' + localStorage.getItem('jwt')
    },
-   body:JSON.stringify({
-    postId:id
-   })
-  }).then(res=>res.json())
-  .then(result=>{
-   const newData = data.map(item=>{
-    if (item._id===result._id){
+   body: JSON.stringify({postId: id})
+  }).then(res => res.json()).then(result => {
+   const newData = data.map(item => {
+    if (item._id === result._id) {
      return result
     } else {
      return item
     }
    })
    setData(newData)
-  }).catch(err=>{
+  }).catch(err => {
+   console.log(err);
+  })
+ }
+ const makeComment = (text, postId) => {
+  fetch('/comment', {
+   method: 'put',
+   headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + localStorage.getItem('jwt')
+   },
+   body: JSON.stringify({postId, text})
+  }).then(res => res.json()).then(result => {
+   const newData = data.map(item => {
+    if (item._id === result._id) {
+     return result
+    } else {
+     return item
+    }
+   })
+   setData(newData)
+  }).catch(err => {
    console.log(err);
   })
  }
@@ -75,14 +90,33 @@ const Home = () => {
       <i className="material-icons" style={{
         color: 'red'
        }}>favorite</i>
-       {item.likes.includes(state._id)
-       ? <i className="material-icons" onClick={()=>{unlikePost(item._id)}}>thumb_down</i>
-       : <i className="material-icons" onClick={()=>{likePost(item._id)}}>thumb_up</i>
+      {
+       item.likes.includes(state._id)
+        ? <i className="material-icons" onClick={() => {
+           unlikePost(item._id)
+          }}>thumb_down</i>
+        : <i className="material-icons" onClick={() => {
+           likePost(item._id)
+          }}>thumb_up</i>
       }
-      <h6>{item.likes.length} likes</h6>
+      <h6>{item.likes.length}
+        &nbsp;likes</h6>
       <h6>{item.title}</h6>
       <p>{item.body}</p>
-      <input type="text" placeholder="Add a comment"/>
+      {
+       item.comments.map(record => {
+        return <h6 key={record._id}>
+         <span style={{
+           fontWeight: '500'
+          }}>{record.postedBy.name}&nbsp;</span>{record.text}</h6>
+       })
+      }
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        makeComment(e.target[0].value, item._id)
+       }}>
+       <input type="text" placeholder="Add a comment"/>
+      </form>
      </div>
     </div>)
    })
--- server/routes/post.js
+++ server/routes/post.js
@@ -7,6 +7,7 @@ const Post = mongoose.model('Post')
 router.get('/allpost', requireLogin, (req, res)=>{
  Post.find()
  .populate('postedBy','_id name')
+ .populate('comments.postedBy','_id name')
  .then(posts=>{
   res.json({posts})
  })
@@ -75,16 +76,18 @@ router.put('/unlike', requireLogin, (req, res)=>{
 })
 
 router.put('/comment', requireLogin, (req, res)=>{
- const commment = {
+ const comment = {
   text: req.body.text,
   postedBy: req.user._id
  }
+ console.log(comment);
  Post.findByIdAndUpdate(req.body.postId,{
   $push:{comments:comment}
  },{
   new:true
  })
  .populate('comments.postedBy','_id name')
+ .populate('postedBy','_id name')
  .exec((err,result)=>{
   if (err) {
    return res.status(422).json({error:err})
