a03e053be54f3d08694f2823ce6e9d2ee436d8bf
Zoilo Granda <zgranda@gmail.com>
2020-05-21 15:11:24 -0400

    Updating profile picture

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -3,7 +3,9 @@ import {UserContext} from '../../App'
 
 const Profile =()=>{
  const [mypics, setPics] = useState([])
- const {state} = useContext(UserContext)
+ const {state, dispatch} = useContext(UserContext)
+ const [image, setImage] = useState('');
+ const [url, setUrl] = useState('');
  console.log({state});
  useEffect(()=>{
   fetch('/mypost',{
@@ -15,13 +17,37 @@ const Profile =()=>{
    setPics(result.mypost)
   })
  },[])
+ useEffect(()=>{
+  if (image) {
+   const data = new FormData();
+   data.append('file', image)
+   data.append('upload_preset', 'instaclone')
+   data.append('cloud_name', 'zgranda')
+   fetch('https://api.cloudinary.com/v1_1/zgranda/image/upload', {
+    method: 'post',
+    body: data
+   }).then(res => res.json()).then(data => {
+    setUrl(data.url)
+    localStorage.setItem('user',JSON.stringify({...state,pic:data.url}))
+    dispatch({type:'UPDATEPIC', payload: data.url})
+   }).catch(err => {
+    console.log(err);
+   })
+  }
+ },[image])
+ const updatePhoto=(file)=>{
+  setImage(file)
+ }
  return(
   <div style={{maxWidth:"550px", margin:"0px auto"}}>
+  <div style={{
+   margin:"18px 0px",
+   borderBottom:"1px solid grey"
+  }}>
+
    <div style={{
     display: "flex",
-    justifyContent: "space-around",
-    margin:"18px 0px",
-    borderBottom:"1px solid grey"
+    justifyContent: "space-around"
    }}>
     <div>
      <img style={{width:"160px", height:"160px", borderRadius:"80px"}}
@@ -37,6 +63,16 @@ const Profile =()=>{
      </div>
     </div>
    </div>
+   <div className="file-field input-field" style={{margin : "10px"}}>
+    <div className="btn blue darken-1">
+     <span>Update Photo</span>
+     <input type="file" multiple="multiple" onChange={(e) => updatePhoto(e.target.files[0])}/>
+    </div>
+    <div className="file-path-wrapper">
+     <input className="file-path validate" type="text" placeholder="Upload one or more files"/>
+    </div>
+   </div>
+  </div>
    <div className="gallery">
    {
     mypics.map(item=>{
