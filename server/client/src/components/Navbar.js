5771c4aa7db5a4a3977823f181c05dd39b4b8463
Zoilo Granda <zgranda@gmail.com>
2020-05-27 09:35:34 -0400

    Search users front end

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -1,13 +1,20 @@
-import React, {useContext} from 'react'
+import React, {useContext, useRef, useEffect, useState} from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import {UserContext} from '../App'
+import M from 'materialize-css';
 
 const NavBar = () => {
+ const searchModal = useRef(null)
+ const [search, setSearch] = useState('')
  const {state, dispatch} = useContext(UserContext)
  const history = useHistory()
+ useEffect(()=>{
+  M.Modal.init(searchModal.current)
+ },[])
  const renderList = () => {
   if (state) {
    return [
+    <li key="search"><i data-target="modal1" className="modal-trigger large material-icons" style={{color:'black'}}>search</i></li>,
     <li key="profile">
      <Link to="/profile">Profile</Link>
     </li>,
@@ -22,34 +29,49 @@ const NavBar = () => {
       localStorage.clear()
       dispatch({
        type:'CLEAR'
-     })
-     history.push('/signin')
-    }}
+      })
+      history.push('/signin')
+     }}
      >Logout
-     </button>
-    </li>
-   ]
-  } else {
-   return [
-    <li key="signin">
-     <Link to="/signin">Signin</Link>
-    </li>,
-    <li key="signup">
-     <Link to="/signup">Signup</Link>
-    </li>
-   ]
-  }
+    </button>
+   </li>
+  ]
+ } else {
+  return [
+   <li key="signin">
+    <Link to="/signin">Signin</Link>
+   </li>,
+   <li key="signup">
+    <Link to="/signup">Signup</Link>
+   </li>
+  ]
  }
- return (<nav>
-  <div className="nav-wrapper white" style={{
-    color: "black"
-   }}>
-   <Link to={state
-     ? "/"
-     : "signin"} className="brand-logo left">Instaclone</Link>
-   <ul id="nav-mobile" className="right">
-    {renderList()}
-   </ul>
+}
+return (<nav>
+ <div className="nav-wrapper white" style={{
+  color: "black"
+ }}>
+ <Link to={state
+  ? "/"
+  : "signin"} className="brand-logo left">Instaclone</Link>
+  <ul id="nav-mobile" className="right">
+   {renderList()}
+  </ul>
+  <div id="modal1" className="modal" ref={searchModal} style={{color:'black'}}>
+   <div className="modal-content">
+    <input type="text" placeholder="Search users" value={search}
+     onChange={(e) => setSearch(e.target.value)}/>
+     <ul className="collection">
+      <li className="collection-item">Alvin</li>
+      <li className="collection-item">Alvin</li>
+      <li className="collection-item">Alvin</li>
+      <li className="collection-item">Alvin</li>
+     </ul>
+    </div>
+    <div className="modal-footer">
+     <button className="modal-close waves-effect waves-green btn-flat">Agree</button>
+    </div>
+   </div>
   </div>
  </nav>)
 }
