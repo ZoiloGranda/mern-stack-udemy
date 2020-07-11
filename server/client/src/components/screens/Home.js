a7010344950d9ea14a65af980ee524730e1c55f8
Zoilo Granda <zgranda@gmail.com>
2020-07-11 16:59:23 -0400

    Added spinner while getting posts

--- server/client/src/components/screens/Home.js
+++ server/client/src/components/screens/Home.js
@@ -6,13 +6,16 @@ import './Home.css'
 
 const Home = () => {
  const [data, setData] = useState([])
+ const [showSpinner, setShowSpinner] = useState('inactive')
  const {state} = useContext(UserContext)
  useEffect(() => {
+  setShowSpinner('active')
   fetch('/allpost', {
    headers: {
     Authorization: 'Bearer ' + localStorage.getItem('jwt')
    }
   }).then(res => res.json()).then(result => {
+   setShowSpinner('inactive')
    setData(result.posts)
   })
  }, [])
@@ -24,34 +27,47 @@ const Home = () => {
     Authorization: 'Bearer ' + localStorage.getItem('jwt')
    }
   }).then(res => res.json()).then(result => {
-   const newData = data.filter(item=>{
-    return item._id!== result._id
+   const newData = data.filter(item => {
+    return item._id !== result._id
    })
    setData(newData)
   })
  }
  return (<div className="home">
   {
-   data.map(item => {
-    return (<div className="card home-card" key={item._id}>
-     <h5> <Link to={
-      item.postedBy._id!==state._id
-      ?'/profile/'+item.postedBy._id
-      :'profile'
-     }>{item.postedBy.name}
-     </Link> 
-      {item.postedBy._id===state._id &&
-      <i className="material-icons right"
-       onClick={()=>deletePost(item._id)}>delete</i>
-      }
-     </h5>
-     <div className="card-image">
-      <img src={item.photo} alt="wallpaper"/>
+   showSpinner === 'active'
+    ? <div className="container spinner-container">
+      <div className="col s12">
+       <div className={`preloader-wrapper big ${showSpinner}`}>
+        <div className="spinner-layer spinner-blue-only">
+         <div className="circle-clipper left">
+          <div className="circle"></div>
+         </div>
+         <div className="gap-patch">
+          <div className="circle"></div>
+         </div>
+         <div className="circle-clipper right">
+          <div className="circle"></div>
+         </div>
+        </div>
+       </div>
+      </div>
      </div>
-     <CardContent item={item}/>
- 
-    </div>)
-   })
+    : data.map(item => {
+     return (<div className="card home-card" key={item._id}>
+      <h5>
+       <Link to={item.postedBy._id !== state._id
+         ? '/profile/' + item.postedBy._id
+         : 'profile'}>{item.postedBy.name}
+       </Link>
+       {item.postedBy._id === state._id && <i className="material-icons right" onClick={() => deletePost(item._id)}>delete</i>}
+      </h5>
+      <div className="card-image">
+       <img src={item.photo} alt="wallpaper"/>
+      </div>
+      <CardContent item={item}/>
+     </div>)
+    })
   }
  </div>)
 }
