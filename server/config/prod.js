d13798f323260c6a194ba92a44b3a96d7974a9fa
Zoilo Granda <zgranda@gmail.com>
2020-12-13 12:10:48 -0400

    removes photos when deleting a post

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,5 +5,8 @@ module.exports={
  JWT_SECRET:process.env.JWT_SECRET,
  SENDGRID_KEY:process.env.SENDGRID_KEY,
  RESET_LINK:process.env.RESET_LINK,
- SENDER_EMAIL:process.env.SENDER_EMAIL
+ SENDER_EMAIL:process.env.SENDER_EMAIL,
+ CLOUD_NAME: process.env.CLOUD_NAME,
+ CLOUD_API_KEY: process.env.CLOUD_API_KEY,
+ CLOUD_API_SECRET: process.env.CLOUD_API_SECRET
 }
--- server/routes/post.js
+++ server/routes/post.js
@@ -3,153 +3,167 @@ const router = express.Router()
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const Post = mongoose.model('Post')
+const fetch = require('node-fetch');
+const {CLOUD_NAME, CLOUD_API_KEY, CLOUD_API_SECRET} = require('../config/keys');
 
-router.get('/allpost', requireLogin, (req, res)=>{
+router.get('/allpost', requireLogin, (req, res) => {
  Post.find()
- .populate('postedBy','_id name')
- .populate('comments.postedBy','_id name')
- .sort('-createdAt')
- .then(posts=>{
-  res.json({posts})
- })
- .catch(err=>{
-  console.log(err);
- })
+  .populate('postedBy', '_id name')
+  .populate('comments.postedBy', '_id name')
+  .sort('-createdAt')
+  .then(posts => {
+   res.json({ posts })
+  })
+  .catch(err => {
+   console.log(err);
+  })
 })
 
-router.get('/getsubpost', requireLogin, (req, res)=>{
- Post.find({postedBy:{$in:req.user.following}})
- .populate('postedBy','_id name')
- .populate('comments.postedBy','_id name')
- .sort('-createdAt')
- .then(posts=>{
-  res.json({posts})
- })
- .catch(err=>{
-  console.log(err);
- })
+router.get('/getsubpost', requireLogin, (req, res) => {
+ Post.find({ postedBy: { $in: req.user.following } })
+  .populate('postedBy', '_id name')
+  .populate('comments.postedBy', '_id name')
+  .sort('-createdAt')
+  .then(posts => {
+   res.json({ posts })
+  })
+  .catch(err => {
+   console.log(err);
+  })
 })
 
-router.post('/createpost',requireLogin,(req, res)=>{
- const {title, body, pic} = req.body;
- if (!title||!body ||!pic) {
-  return res.status(422).json({error:'Please add all fields'})
+router.post('/createpost', requireLogin, (req, res) => {
+ const { title, body, pic } = req.body;
+ if (!title || !body || !pic) {
+  return res.status(422).json({ error: 'Please add all fields' })
  }
  req.user.password = undefined;
  const post = new Post({
   title,
   body,
-  photo:pic,
+  photo: pic,
   postedBy: req.user
  })
-  post.save().then(result=>{
-   res.json({post:result})
+ post.save().then(result => {
+   res.json({ post: result })
   })
-  .catch(err=>{
+  .catch(err => {
    console.log(err)
   })
 })
 
-router.get('/mypost', requireLogin, (req, res)=>{
- Post.find({postedBy:req.user._id})
- .populate('postedBy', '_id name')
- .then(mypost=>{
-  res.json({mypost})
- })
- .catch(err=>{
-  console.log(err);
- })
+router.get('/mypost', requireLogin, (req, res) => {
+ Post.find({ postedBy: req.user._id })
+  .populate('postedBy', '_id name')
+  .then(mypost => {
+   res.json({ mypost })
+  })
+  .catch(err => {
+   console.log(err);
+  })
 })
 
-router.put('/like', requireLogin, (req, res)=>{
- Post.findByIdAndUpdate(req.body.postId,{
-  $push:{likes:req.user._id}
- },{
-  new:true
- })
- .populate('comments.postedBy','_id name')
- .exec((err,result)=>{
-  if (err) {
-   return res.status(422).json({error:err})
-  }else {
+router.put('/like', requireLogin, (req, res) => {
+ Post.findByIdAndUpdate(req.body.postId, {
+   $push: { likes: req.user._id }
+  }, {
+   new: true
+  })
+  .populate('comments.postedBy', '_id name')
+  .exec((err, result) => {
+   if (err) {
+    return res.status(422).json({ error: err })
+   } else {
     res.json(result)
-  }
- })
+   }
+  })
 })
 
-router.put('/unlike', requireLogin, (req, res)=>{
- Post.findByIdAndUpdate(req.body.postId,{
-  $pull:{likes:req.user._id}
- },{
-  new:true
- })
- .populate('comments.postedBy','_id name')
- .exec((err,result)=>{
-  if (err) {
-   return res.status(422).json({error:err})
-  }else {
+router.put('/unlike', requireLogin, (req, res) => {
+ Post.findByIdAndUpdate(req.body.postId, {
+   $pull: { likes: req.user._id }
+  }, {
+   new: true
+  })
+  .populate('comments.postedBy', '_id name')
+  .exec((err, result) => {
+   if (err) {
+    return res.status(422).json({ error: err })
+   } else {
     res.json(result)
-  }
- })
+   }
+  })
 })
 
-router.put('/comment', requireLogin, (req, res)=>{
+router.put('/comment', requireLogin, (req, res) => {
  const comment = {
   text: req.body.text,
   postedBy: req.user._id
  }
  console.log(comment);
- Post.findByIdAndUpdate(req.body.postId,{
-  $push:{comments:comment}
- },{
-  new:true
- })
- .populate('comments.postedBy','_id name')
- .populate('postedBy','_id name')
- .exec((err,result)=>{
-  if (err) {
-   return res.status(422).json({error:err})
-  }else {
+ Post.findByIdAndUpdate(req.body.postId, {
+   $push: { comments: comment }
+  }, {
+   new: true
+  })
+  .populate('comments.postedBy', '_id name')
+  .populate('postedBy', '_id name')
+  .exec((err, result) => {
+   if (err) {
+    return res.status(422).json({ error: err })
+   } else {
     res.json(result)
-  }
- })
+   }
+  })
 })
 
-router.delete('/deletecomment', requireLogin, (req, res)=>{
-console.log(req.body);
- Post.findByIdAndUpdate(req.body.postId,{
-  $pull:{comments:{_id:req.body.commentId}}
- },{
-  new:true
- })
- .populate('comments.postedBy','_id name')
- .exec((err,result)=>{
-  if (err) {
-   console.log(err);
-   return res.status(422).json({error:err})
-  }else {
-   console.log(result);
-    res.json(result)
-  }
- })
-})
-
-router.delete('/deletepost/:postId', requireLogin,(req, res)=>{
- Post.findOne({_id: req.params.postId})
- .populate('postedBy','._id')
- .exec((err,post)=>{
-  if(err||!post){
-   return res.status(422).json({error:err})
-  }
-  if (post.postedBy._id.toString() === req.user._id.toString()) {
-   post.remove()
-   .then(result=>{
-    res.json(result)
-   }).catch(err=>{
+router.delete('/deletecomment', requireLogin, (req, res) => {
+ console.log(req.body);
+ Post.findByIdAndUpdate(req.body.postId, {
+   $pull: { comments: { _id: req.body.commentId } }
+  }, {
+   new: true
+  })
+  .populate('comments.postedBy', '_id name')
+  .exec((err, result) => {
+   if (err) {
     console.log(err);
-   })
-  }
- })
+    return res.status(422).json({ error: err })
+   } else {
+    console.log(result);
+    res.json(result)
+   }
+  })
 })
+
+router.delete('/deletepost/:postId', requireLogin, async (req, res) => {
+ Post.findOne({ _id: req.params.postId })
+  .populate('postedBy', '._id')
+  .exec(async (err, post) => {
+   if (err || !post) {
+    return res.status(422).json({ error: err })
+   }
+   if (post.postedBy._id.toString() === req.user._id.toString()) {
+    await removePhoto(post.photo.split("/").pop())
+    post.remove()
+     .then(result => {
+      res.json(result)
+     }).catch(err => {
+      console.log(err);
+     })
+   }
+  })
+})
+
+function removePhoto(photoName) {
+ const photoId = photoName.substring(0, photoName.lastIndexOf('.'));
+ return fetch(`https://${CLOUD_API_KEY}:${CLOUD_API_SECRET}@api.cloudinary.com/v1_1/${CLOUD_NAME}/resources/image/upload`, {
+  body: `public_ids[]=${photoId}`,
+  headers: {
+   "Content-Type": "application/x-www-form-urlencoded"
+  },
+  method: "DELETE"
+ }).then(result=> result.json())
+}
 
 module.exports = router
